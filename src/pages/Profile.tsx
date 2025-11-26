12b3da15524b2d022596a6b8fb22e9944720eda9
MeoMapOnline <muncute0377392767@gmail.com>
2025-11-26 23:01:46 +0700

    Create  Profile.tsx

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile: React.FC = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Hồ sơ cá nhân</h1>
+      <p>Thông tin người dùng...</p>
+    </div>
+  );
+};
+
+export default Profile;
